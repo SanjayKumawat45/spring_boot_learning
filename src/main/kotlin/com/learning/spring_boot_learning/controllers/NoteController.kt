398a83eb1e4db494161246be45e9a2e4b1aeda6f
SanjayKumawat45 <sanjay4kumawat@gmail.com>
2025-08-21 23:33:41 +0530

    commit

---
+++ src/main/kotlin/com/learning/spring_boot_learning/controllers/NoteController.kt
@@ -0,0 +1,4 @@
+package com.learning.spring_boot_learning.controllers
+
+class NoteController {
+}
--- src/main/kotlin/com/learning/spring_boot_learning/database/repository/NoteRepository.kt
+++ src/main/kotlin/com/learning/spring_boot_learning/database/repository/NoteRepository.kt
@@ -5,6 +5,7 @@ import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface NoteRepository: MongoRepository<Note, ObjectId> {
+    fun findByOwnerId(ownerId: ObjectId): List<Note>
 
 
 }
