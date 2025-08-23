bbe0743c9a70d7ec3091af055efc238faccaeea2
SanjayKumawat45 <sanjay4kumawat@gmail.com>
2025-08-23 15:40:02 +0530

    controller commit

--- src/main/kotlin/com/learning/spring_boot_learning/controllers/NoteController.kt
+++ src/main/kotlin/com/learning/spring_boot_learning/controllers/NoteController.kt
@@ -7,6 +7,7 @@ import io.jsonwebtoken.lang.Objects
 import org.bson.types.ObjectId
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -23,7 +24,6 @@ class NoteController(
         val title: String,
         val content: String,
         val color: Long,
-        val ownerId: String,
     )
 
     data class NoteResponse(
@@ -35,7 +35,9 @@ class NoteController(
     )
 
     @PostMapping
-    fun save(body: NoteRequest): NoteResponse{
+    fun save(
+        @RequestBody body: NoteRequest
+    ): NoteResponse{
         val note = repository.save(
                 Note(
                     id = body.id?.let{ ObjectId(it)}?:ObjectId.get(),
@@ -43,7 +45,7 @@ class NoteController(
                     content = body.content,
                     color = body.color,
                     createdAt = Instant.now(),
-                    ownerId = ObjectId(body.ownerId)
+                    ownerId = ObjectId()
                 )
             )
 
